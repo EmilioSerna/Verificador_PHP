f63bfd9e694dc93a13258bd2e058e7ac0f822bdf
Ángel Emilio Serna Navarro <50151743+Zelvax42@users.noreply.github.com>
2021-11-08 22:15:00 -0700

    mostrar_producto now does the job

--- mostrar_producto.php
+++ mostrar_producto.php
@@ -1,21 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
     <script type="text/javascript">
         setTimeout(function() {
             window.location.href = "index.html";
         }, 3000);
     </script>
+
+    <script type="text/javascript">
+        if (window.addEventListener) {
+            var codigo = "";
+            window.addEventListener("keydown", function(e) {
+                codigo += String.fromCharCode(e.keyCode);
+                if (e.keyCode == 13) {
+                    window.location = "mostrar_producto.php?codigo=" + codigo;
+                    codigo = "";
+                }
+            }, true);
+        }
+    </script>
 </head>
+
 <body>
-    <?php
-        print_r($_GET);
-        $query = "SELECT * FROM productos WHERE producto_codigo = ".$_GET["codigo"].";";
-        echo($query);
-    ?>
+    <h1 style='text-align: center'>
+
+        <?php
+        include("./inc/settings.php");
+
+        try {
+            $codigo = $_GET["codigo"];
+
+            $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $stmt = $conn->prepare("SELECT * FROM productos WHERE producto_codigo = $codigo");
+            $stmt->execute();
+
+            // Set the resulting array to associative
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            $rows = $stmt->rowCount();
+
+            if ($rows == 1) {
+        ?>
+                Producto <?= $result['producto_nombre'] ?><br>
+                Precio <?= $result['producto_precio'] ?><br>
+                Cantidad <?= $result['producto_stock'] ?><br>
+                <img src="<?= $result['producto_imagen'] ?>" width="150px" height="150px">
+            <?php
+            } else {
+            ?>
+                No se encuentra el producto<br>
+                <img src="./img/error.png" alt="" width="50%" height="50%">
+        <?php
+            }
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        ?>
+    </h1>
 </body>
 </html>
